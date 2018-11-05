2f09cb88a131756e9cdb45ce12a7647c246b5908
tbq Y <1256034437@qq.com>
2018-11-05 19:59:54 +0800

    测试类2

---
+++ src/com/itcast_01/demo_01.java
@@ -0,0 +1,11 @@
+package com.itcast_01;
+
+public class demo_01 {
+    public static void main(String[] args) {
+        int sum = 1;
+        for(int a = 1 ;a<=10;a++){
+            sum *= a;
+        }
+        System.out.println(sum);
+    }
+}
